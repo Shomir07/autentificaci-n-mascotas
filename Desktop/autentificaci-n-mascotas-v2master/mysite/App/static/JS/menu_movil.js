5fd15a74b63d5e80467a7b0ad48dcd0bbf943bf5
Shomir07 <80559841+Shomir07@users.noreply.github.com>
2024-09-29 21:41:56 -0500

    Delete Desktop/autentificaci-n-mascotas-v2master directory

--- Desktop/autentificaci-n-mascotas-v2master/mysite/App/static/JS/menu_movil.js
+++
@@ -1,8 +0,0 @@
-document.querySelector('.profile-pic').addEventListener('click', function() {
-    var navbar = document.querySelector('.l-navbar_movil');
-    if (navbar.classList.contains('show')) {
-        navbar.classList.remove('show');
-    } else {
-        navbar.classList.add('show');
-    }
-});
--- Desktop/autentificaci-n-mascotas-v2master/mysite/mysite/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    uid = models.CharField(max_length=255, unique=True)
-    photo_url = models.URLField(blank=True, null=True)
